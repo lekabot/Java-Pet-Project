47a6d8ee221b1bd80577d50e94daed143afd0890
Ilya Lopatin <mrmochin@yandex.ru>
2024-10-02 21:10:51 +0700

    Add simple structure

---
+++ Pastebin/App/src/main/java/edu/school21/app/AppApplication.java
@@ -0,0 +1,13 @@
+package edu.school21.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
